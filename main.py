5cc72e3ef71499349aa25a57890df4398f49181f
MauPxt <90274764+MauPxt@users.noreply.github.com>
2022-06-09 03:57:06 -0300

    V1 - Database
    Modificando a API e pedindo para a mesma efetuar uma requisição no banco de dados do robô de apostas afim de calcular o score geral

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
 # Importando o FastAPI
 from fastapi import FastAPI
+import mysql.connector
+from decouple import config
+import sys
+
+# Conectando com o Banco de dados SQL
+try:
+    connection = mysql.connector.connect(
+        host=config('host'),
+        database=config('database'),
+        user=config('user'),
+        password=config('password')
+    )
+    cursor = connection.cursor()
+    cursor.execute('''
+SELECT 
+resultado, COUNT(resultado)
+FROM
+score
+GROUP BY resultado;
+''')
+    result = cursor.fetchall()
+    result = dict(result)
+except Exception as e:
+    print(e)
+    sys.exit()
+
 
 # Criando uma instância para o FastAPI
 app = FastAPI()
 
+
 # Criando o decorator de rota do operator get
+@app.get('/all')
+def score_geral():  # Função da rota
+    return result
 
 
-@app.get('/')
-def hello_root():  # Função da rota
-    return {'message': 'hello world'}
+@app.get('/green')
+def score_green():
+    return {'greens': result['GREEN'] + result['GREEN GALE 1'] + result['GREEN GALE 2']}
+
+
+@app.get('/red')
+def score_red():
+    return {'reds': result['RED']}
